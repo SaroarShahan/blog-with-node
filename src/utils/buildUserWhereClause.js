16f9db0115f93dabf2ca8e050ccb75b7504b12c0
SaroarShahan <saroar.shahan@gmail.com>
2026-03-10 01:19:03 +0600

    feat: add build where clause util func for users

---
+++ src/utils/buildUserWhereClause.js
@@ -0,0 +1,37 @@
+import { Op } from "sequelize";
+
+export const buildUserWhereClause = (query) => {
+    const whereClause = {};
+
+    if (query.email) {
+        whereClause.email = {
+            [Op.like]: `%${query.email}%`
+        };
+    }
+
+    if (query.firstName) {
+        whereClause.firstName = {
+            [Op.like]: `%${query.firstName}%`
+        };
+    }
+
+    if (query.lastName) {
+        whereClause.lastName = {
+            [Op.like]: `%${query.lastName}%`
+        };
+    }
+
+    if (query.gender) {
+        whereClause.gender = {
+            [Op.like]: `%${query.gender}%`
+        };
+    }
+
+    if (query.role) {
+        whereClause.role = {
+            [Op.like]: `%${query.role}%`
+        };
+    }
+
+    return whereClause;
+};
