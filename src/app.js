1a25169cbcfcda832a28fc197024951dfb8f8c40
SaroarShahan <saroar.shahan@gmail.com>
2026-03-10 00:54:36 +0600

    feat: add user routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const userRoutes = require('./routes/userRoutes');
+
 function createApp() {
   const app = express();
 
   app.use(express.json());
 
   // Routes
+  app.use('/api/v1/users', userRoutes);
+
   app.get('/health', (req, res) => {
     res.json({
       uptime: process.uptime(),
