51dc335aa262679bdc71a92bc6939379e3cc7036
SaroarShahan <saroar.shahan@gmail.com>
2026-03-11 00:05:34 +0600

    feat: add jwt utils for generate token, verify token, and decode token

---
+++ src/utils/jwt.js
@@ -0,0 +1,46 @@
+const { createSecretKey } = require("crypto")
+const { SignJWT, jwtVerify, decodeJwt } = require('jose');
+
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+
+exports.generateToken = async (payload) => {
+    const secret = config.jwtSecret
+
+    if (!secret) {
+        throw new Error('JWT_SECRET environment variable is not set')
+    }
+
+    const secretKey = createSecretKey(secret, 'utf-8')
+
+    return await new SignJWT(payload)
+        .setProtectedHeader({ alg: 'HS256' })
+        .setIssuedAt()
+        .setExpirationTime(config.JWT_EXPIRES_IN || '7d')
+        .sign(secretKey)
+};
+
+exports.verifyToken = async (token) => {
+    const secretKey = createSecretKey(config.jwtSecret, 'utf-8')
+    const { payload } = await jwtVerify(token, secretKey)
+
+    return {
+        id: payload.id,
+        email: payload.email,
+    }
+};
+
+exports.decodeToken = async (token) => {
+    try {
+        const payload = decodeJwt(token)
+
+        return {
+            id: payload.id,
+            email: payload.email,
+            username: payload.username,
+        }
+    } catch {
+        return null
+    }
+};
