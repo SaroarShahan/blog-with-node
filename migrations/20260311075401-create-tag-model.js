e902a8c788bb5f703ce4300e1feb13d3ad3d283a
SaroarShahan <saroar.shahan@gmail.com>
2026-03-13 00:17:01 +0600

    feat: add tag migration

---
+++ migrations/20260311075401-create-tag-model.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tags', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        field: 'name',
+        allowNull: false,
+        unique: true,
+      },
+      slug: {
+        type: Sequelize.STRING(120),
+        field: 'slug',
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        field: 'updated_at',
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tags');
+  },
+};
