c789841b9db0248afdfbf54a01f1c0e04689d281
SaroarShahan <saroar.shahan@gmail.com>
2026-03-13 00:16:39 +0600

    feat: add tag controller

--- src/controllers/tagController.js
+++ src/controllers/tagController.js
@@ -1,20 +1,20 @@
 const { Op } = require('sequelize');
 
 const asyncHandler = require('../utils/asyncHandler');
-const { CategoryModel } = require('../models');
+const { TagModel } = require('../models');
 const urlSlugify = require('../utils');
 
 exports.createTag = asyncHandler(async (req, res) => {
   const { name } = req.body;
   let slug = urlSlugify(name);
 
-  const existingSlug = await CategoryModel.findOne({ where: { slug } });
+  const existingSlug = await TagModel.findOne({ where: { slug } });
 
   if (existingSlug) {
     slug = `${slug}-${Date.now()}`;
   }
 
-  const existingTag = await CategoryModel.findOne({ where: { name } });
+  const existingTag = await TagModel.findOne({ where: { name } });
 
   if (existingTag) {
     return res.status(400).json({
@@ -23,11 +23,105 @@ exports.createTag = asyncHandler(async (req, res) => {
     });
   }
 
-  const tag = await CategoryModel.create({ name, slug });
+  const tag = await TagModel.create({ name, slug });
 
   return res.status(201).json({
     success: true,
     message: 'Tag has been created successfully',
     data: tag,
+  });
+});
+
+exports.getAllTags = asyncHandler(async (req, res) => {
+  const tags = await TagModel.findAll({
+    order: [['id', 'DESC']],
+  });
+
+  return res.status(200).json({
+    status: true,
+    message: 'Tags fetched successfully',
+    data: tags,
+  });
+});
+
+exports.getTag = asyncHandler(async (req, res) => {
+  const { idOrSlug } = req.params;
+
+  const tag = await TagModel.findOne({
+    where: {
+      [Op.or]: [{ slug: idOrSlug }, { id: idOrSlug }],
+    },
+  });
+
+  if (!tag) {
+    return res.status(404).json({
+      success: false,
+      message: 'Tag not found',
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: 'Tag fetched successfully',
+    data: tag,
+  });
+});
+
+exports.updateTag = asyncHandler(async (req, res) => {
+  const { idOrSlug } = req.params;
+  const { name } = req.body;
+
+  const tag = await TagModel.findOne({
+    where: {
+      [Op.or]: [{ slug: idOrSlug }, { id: idOrSlug }],
+    },
+  });
+
+  if (!tag) {
+    return res.status(404).json({
+      success: false,
+      message: 'Tag not found',
+    });
+  }
+
+  let existingSlug = tag.slug;
+
+  if (name && name !== tag.name) {
+    existingSlug = urlSlugify(name);
+  }
+
+  await tag.update({
+    name: name ?? tag.name,
+    slug: existingSlug,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: 'Tag has been updated successfully',
+    data: tag,
+  });
+});
+
+exports.deleteTag = asyncHandler(async (req, res) => {
+  const { idOrSlug } = req.params;
+
+  const tag = await TagModel.findOne({
+    where: {
+      [Op.or]: [{ slug: idOrSlug }, { id: idOrSlug }],
+    },
+  });
+
+  if (!tag) {
+    return res.status(404).json({
+      success: false,
+      message: 'Tag not found',
+    });
+  }
+
+  await tag.destroy();
+
+  return res.status(200).json({
+    success: true,
+    message: 'Tag has been deleted successfully',
   });
 });
