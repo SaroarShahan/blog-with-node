72eee213f09117a690ecc032f759f18468da5fbb
SaroarShahan <saroar.shahan@gmail.com>
2026-03-12 23:11:54 +0600

    feat: add category migration

---
+++ migrations/20260311073203-create-category-model.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.BIGINT.UNSIGNED,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        field: 'name',
+        allowNull: false,
+        unique: true,
+      },
+      slug: {
+        type: Sequelize.STRING(120),
+        field: 'slug',
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        field: 'description',
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        field: 'updated_at',
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('categories');
+  },
+};
