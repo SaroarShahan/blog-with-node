4c9f81320d0a33d6ee782ad082bd025eaf717ba5
SaroarShahan <saroar.shahan@gmail.com>
2026-03-12 23:08:39 +0600

    feat: update buildUserWhereClause utils

--- src/utils/buildUserWhereClause.js
+++ src/utils/buildUserWhereClause.js
@@ -1,39 +1,33 @@
-const { Op } = require("sequelize");
+const { Op } = require('sequelize');
 
 const buildUserWhereClause = (query) => {
-    const whereClause = {};
+  const whereClause = {};
 
-    if (query.email) {
-        whereClause.email = {
-            [Op.like]: `%${query.email}%`
-        };
-    }
+  if (query.email) {
+    whereClause.email = {
+      [Op.like]: `%${query.email}%`,
+    };
+  }
 
-    if (query.firstName) {
-        whereClause.firstName = {
-            [Op.like]: `%${query.firstName}%`
-        };
-    }
+  if (query.username) {
+    whereClause.username = {
+      [Op.like]: `%${query.username}%`,
+    };
+  }
 
-    if (query.lastName) {
-        whereClause.lastName = {
-            [Op.like]: `%${query.lastName}%`
-        };
-    }
+  if (query.gender) {
+    whereClause.gender = {
+      [Op.like]: `%${query.gender}%`,
+    };
+  }
 
-    if (query.gender) {
-        whereClause.gender = {
-            [Op.like]: `%${query.gender}%`
-        };
-    }
+  if (query.role) {
+    whereClause.role = {
+      [Op.like]: `%${query.role}%`,
+    };
+  }
 
-    if (query.role) {
-        whereClause.role = {
-            [Op.like]: `%${query.role}%`
-        };
-    }
-
-    return whereClause;
+  return whereClause;
 };
 
 module.exports = { buildUserWhereClause };
