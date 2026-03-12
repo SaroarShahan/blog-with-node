631f3ffc27436bab6b26dc64362f2c1178726520
SaroarShahan <saroar.shahan@gmail.com>
2026-03-12 23:09:47 +0600

    feat: add urlSlugify

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+module.exports = function urlSlugify(text) {
+  return text
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/&/g, 'and')
+    .replace(/[^\w\s-]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-');
+};
