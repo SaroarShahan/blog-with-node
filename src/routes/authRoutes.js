e19d94aacb980e1ea07b2c319130843e7fa2fd74
SaroarShahan <saroar.shahan@gmail.com>
2026-03-15 03:37:53 +0600

    feat: add schema validation for authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const { register, login } = require('../controllers/authController');
+const { validate } = require('../middleware');
+const { registerSchema, loginSchema } = require('../validations/authValidation');
 
 const router = express.Router();
 
-router.route('/register').post(register);
-router.route('/login').post(login);
+router.route('/register').post(validate(registerSchema), register);
+router.route('/login').post(validate(loginSchema), login);
 
 module.exports = router;
