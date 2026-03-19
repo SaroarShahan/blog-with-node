2a5cf3cf46d395b2ab6de389ec3ddf930a2f06e3
SaroarShahan <saroar.shahan@gmail.com>
2026-03-20 02:03:04 +0600

    delete: error handler

--- src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-exports.errorHandler = (err, req, res, _next) => {
-  console.error(err);
-
-  return res.status(500).json({
-    success: false,
-    message: err.message || 'Internal server error',
-  });
-};
