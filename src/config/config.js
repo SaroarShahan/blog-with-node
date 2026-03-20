acac791a2574309e41ddb10234c4016e5d168694
SaroarShahan <saroar.shahan@gmail.com>
2026-03-21 01:42:06 +0600

    chore: update config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 
 const sequelizeLogging = process.env.DB_LOGGING === 'true' ? console.log : false;
+const sharedConfig = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: sequelizeLogging,
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+};
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: sequelizeLogging,
-    jwtSecret: process.env.JWT_SECRET,
-    jwtExpiresIn: process.env.JWT_EXPIRES_IN,
-  },
+  development: sharedConfig,
+  production: sharedConfig,
 };
